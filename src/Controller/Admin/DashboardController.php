4212ec4bbdaf1ac463e9de247ef86e72ea5f1541
Charly Guerin <lannister135@gmail.com>
2023-05-11 13:50:07 +0200

    fix easyadmin

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,18 +2,13 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Client;
-use App\Entity\Horaires;
-use App\Entity\Reservation;
-use App\Entity\Photo;
-use App\Entity\Plat;
+use App\Entity\User;
+
 
 class DashboardController extends AbstractDashboardController
 {
@@ -33,6 +28,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-list', UserClass::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-list', User::class);
     }
 }
